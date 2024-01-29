26f762ad18b55b1d14134c442cab68bad360e662
Marcio Curvello <marciocurvello@gmail.com>
2024-01-29 17:11:17 -0300

    add endpoints

--- ScreenSound.ScreenSoundAPI/Program.cs
+++ ScreenSound.ScreenSoundAPI/Program.cs
@@ -1,18 +1,20 @@
 ﻿using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc;
+using ScreenSound.ScreenSoundAPI.Endpoints;
 using ScreenSound.Shared.Dados;
 using ScreenSound.Shared.Modelos;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<ScreenSoundContext>();
+builder.Services.AddTransient<DAL<Artista>>();
+builder.Services.AddTransient<DAL<Musica>>();
+
 builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
 var app = builder.Build();
 
-app.MapGet("/", () =>
-{
-    var dal = new DAL<Artista>(new ScreenSoundContext());
-    return dal.Listar();
-}
-);
+app.AddEndPointsArtistas();
+app.AddEndPointsMusicas();
 
 app.Run();
 
